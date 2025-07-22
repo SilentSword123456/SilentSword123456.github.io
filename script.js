82f33653d98b31b3629b93a85e13c9cd14c8b57a
Andrei Dumitru <andrei925.dumitru@gmail.com>
2025-07-22 16:40:27 +0300

    added random particles intervals

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function sayHello(){
-    print("Hello from SilentLab!")
-}
+setInterval(() => {
+    // Pick a random particle (1-5)
+    const randomParticle = Math.floor(Math.random() * 5) + 1;
+    const particle = document.querySelector(`.particle:nth-child(${randomParticle})`);
+    
+    // Trigger animation by adding/removing a class
+    particle.classList.remove('fire');
+    setTimeout(() => particle.classList.add('fire'), 10);
+}, 1200);
